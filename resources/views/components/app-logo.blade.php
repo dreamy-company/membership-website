52409a4a76d98b7f7ae4a1230f780faac40be6a3
ekanata14 <ekanata1411@gmail.com>
2025-12-08 22:04:40 +0800

    feat: fixing white mode

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Membership Website</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold text-black dark:text-white">Membership Website</span>
 </div>
