aff24eb47cfec46d56760e3183e5f10c36b0850b
Tio Mahesa <tiomahesa.work@gmail.com>
2026-01-27 17:39:47 +0800

    feat: replace standard buttons with button-icon components for editing and deleting transactions

--- resources/views/livewire/admin/transactions/index.blade.php
+++ resources/views/livewire/admin/transactions/index.blade.php
@@ -52,8 +52,8 @@
                             <x-table.td>{{ number_format($item->balance) }}</x-table.td>
                             <x-table.td>{{ number_format($item->bonus) }}</x-table.td>
                             <x-table.td>
-                                <x-widget.button color="neutral" name="Edit" action="openModal({{ $item->id }})" />
-                                <x-widget.button color="danger" name="Delete" action="confirmDelete({{ $item->id }})" />
+                                <x-widget.button-icon type="edit" action="openModal({{ $item->id }})" />
+                                <x-widget.button-icon type="delete" action="confirmDelete({{ $item->id }})" />
                             </x-table.td>
                         </x-table.tr>
                     @empty
