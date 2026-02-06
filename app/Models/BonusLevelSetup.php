996a7f7922b5858cebdba0552ce1a780e911ca58
Tio Mahesa <tiomahesa.work@gmail.com>
2026-02-06 19:18:47 +0800

    feat: update BonusSettings to use BonusLevelSetup model and adjust properties for bonus settings

--- app/Models/BonusLevelSetup.php
+++ app/Models/BonusLevelSetup.php
@@ -8,4 +8,13 @@ class BonusLevelSetup extends Model
 {
     protected $guarded = ['id'];
 
+    // ini function search yang ada di livewire
+    public function scopeSearch($query, $term)
+    {
+        if ($term) {
+            $query->where('kodeBonus', 'like', "%{$term}%")
+                ->orWhere('persenBonus', 'like', "%{$term}%");
+        }
+    }
+
 }
